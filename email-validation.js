bc5a5b309fbcedc2504032dccb45912b2b04f71d
Ryan Sheehan <ryanjsheehan1@gmail.com>
2021-09-08 16:17:08 -0500

    Initital commit

--- email-validation.js
+++ email-validation.js
@@ -1,10 +1,17 @@
-function ValidateEmail(inputText) {
-  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (inputText.value.match(mailformat)) {
-    return true;
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const errorElement = document.querySelector(".error");
+const regex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+form.addEventListener("submit", (e) => {
+  const test = email.value.length === 0 || regex.test(email.value);
+
+  if (!test) {
+    e.preventDefault();
+    errorElement.innerText = "Oops! Please check your email";
   } else {
-    alert("Oops! Please check your email");
-    document.getElementsByClassName("email-warning").innerHTML = "TEST";
-    return false;
+    e.preventDefault();
+    // errorElement.innerText = "valid";
   }
-}
+});
