cab028a1b8534da6bacc9a42d1457c1b7ee80149
Ryan Sheehan <ryanjsheehan1@gmail.com>
2021-09-10 16:41:24 -0500

    Added condition for empty parameter and dynamic styling

--- email-validation.js
+++ email-validation.js
@@ -5,13 +5,22 @@ const regex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 form.addEventListener("submit", (e) => {
-  const test = email.value.length === 0 || regex.test(email.value);
+  const test = regex.test(email.value);
 
-  if (!test) {
+  if (!test && email.value !== "") {
     e.preventDefault();
+    errorElement.style.color = "#fb3e3e";
     errorElement.innerText = "Oops! Please check your email";
+    form.style.paddingBottom = "3.6rem";
+  } else if (email.value === "") {
+    e.preventDefault();
+    errorElement.innerText = "";
+    form.style.paddingBottom = "6.4rem";
   } else {
     e.preventDefault();
-    // errorElement.innerText = "valid";
+    errorElement.style.color = "#54e6af";
+    errorElement.innerText =
+      "Thanks! Your request for access has been submitted";
+    form.style.paddingBottom = "3.6rem";
   }
 });
